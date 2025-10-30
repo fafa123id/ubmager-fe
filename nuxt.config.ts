85d2bd70ddd9a5a67d82ef2c44cb2a7779bcf679
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-10-31 06:37:59 +0700

    Add Tailwind CSS integration and configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  css: ["./app/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  ssr: true,
+  nitro: {
+    preset: "node",
+  },
+  app: {
+    head: {
+      title: "UBMager Frontend",
+      meta: [{ name: "description", content: "Frontend SSR untuk UBMager" }],
+    },
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+    },
+  },
+});
