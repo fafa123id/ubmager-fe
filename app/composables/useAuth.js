4a11d11a9dd31c6ac6c60a3527fac7e07873dd13
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-10-31 16:21:27 +0700

    Refactor useAuth and api plugin for improved readability and consistency

--- app/composables/useAuth.js
+++ app/composables/useAuth.js
@@ -1,30 +1,45 @@
 // composables/useAuth.ts
 export const useAuth = () => {
-  const user = useState('user', () => null)
+  const user = useState("user", () => null);
 
   async function fetchUser() {
-    try { user.value = await $fetch('api/user', { baseURL: 'https://api.ubmager.bornhub.cloud', credentials: 'include' }) }
-    catch { user.value = null }
+    try {
+      user.value = await $fetch("api/user", {
+        baseURL: "https://api.ubmager.bornhub.cloud",
+        credentials: "include",
+      });
+    } catch {
+      user.value = null;
+    }
   }
 
   async function login(email, password) {
-
-    await $fetch('/sanctum/csrf-cookie', { baseURL: 'https://api.ubmager.bornhub.cloud', credentials: 'include' })
-    // 2) kirim kredensial (Axios/$fetch akan kirim X-XSRF-TOKEN otomatis jika pakai Axios; untuk $fetch, set header manual jika perlu)
-    await $fetch('api/login', {
-      baseURL: 'https://api.ubmager.bornhub.cloud',
-      method: 'POST',
-      body: { email, password },
-      credentials: 'include',
-      headers: { 'X-XSRF-TOKEN': useCookie('XSRF-TOKEN').value }, // jika perlu
-    })
-    await fetchUser()
+    await $fetch("/sanctum/csrf-cookie", {
+      baseURL: "https://api.ubmager.bornhub.cloud",
+      credentials: "include",
+    });
+  
+    await $fetch("api/login", {
+      baseURL: "https://api.ubmager.bornhub.cloud",
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      credentials: "include",
+      headers: { "X-XSRF-TOKEN": useCookie("XSRF-TOKEN").value }, // jika perlu
+    });
+    await fetchUser();
   }
 
   async function logout() {
-    await $fetch('api/logout', { baseURL: 'https://api.ubmager.bornhub.cloud', method: 'POST', credentials: 'include' })
-    user.value = null
+    await $fetch("api/logout", {
+      baseURL: "https://api.ubmager.bornhub.cloud",
+      method: "POST",
+      credentials: "include",
+    });
+    user.value = null;
   }
 
-  return { user, fetchUser, login, logout }
-}
+  return { user, fetchUser, login, logout };
+};
--- app/plugins/api.js
+++ app/plugins/api.js
@@ -1,8 +1,13 @@
 // plugins/api.ts
 export default defineNuxtPlugin(() => {
   const api = $fetch.create({
-    baseURL: 'https://api.ubmager.bornhub.cloud',
-    credentials: 'include', // <== penting supaya kirim cookie
-  })
-  return { provide: { api } }
-})
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-XSRF-TOKEN": useCookie("XSRF-TOKEN"), // ambil dari cookie yang diset Laravel
+    },
+    baseURL: "https://api.ubmager.bornhub.cloud",
+    credentials: "include", // <== penting supaya kirim cookie
+  });
+  return { provide: { api } };
+});
