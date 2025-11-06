67acf8176d68922181491780abe073292880487a
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-07 02:58:49 +0700

    Remove API interceptor logic for token refresh from Nuxt plugin

--- app/plugins/01.api.js
+++ app/plugins/01.api.js
@@ -15,44 +15,7 @@ export default defineNuxtPlugin(async (nuxtApp) => {
   if (token().value) {
     api.defaults.headers.common["Authorization"] = `Bearer ${token().value}`;
   }
-  await api.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    async (error) => {
-      const originalRequest = error.config;
-
-      if (
-        error.response?.status === 401 &&
-        !originalRequest._retry &&
-        originalRequest.url !== "/api/refresh"
-      ) {
-        originalRequest._retry = true;
-
-        try {
-          console.log("Interceptor: Access token expired. Refreshing token...");
-
-          const response = await api.post("/api/refresh");
-          const newAccessToken = response.data.access_token;
-          token().value = newAccessToken;
-          api.defaults.headers.common[
-            "Authorization"
-          ] = `Bearer ${newAccessToken}`;
-          originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
-          return api(originalRequest);
-        } catch (refreshError) {
-          console.error(
-            "Interceptor: Gagal refresh token. Logout.",
-            refreshError
-          );
-          _clearAuth();
-          return Promise.reject(refreshError);
-        }
-      }
-      return Promise.reject(error);
-    }
-  );
-
+  
   return {
     provide: {
       api: api,
