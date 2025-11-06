d892eebc7b4a841a8fc506f6dd35ff6f1d7dc39e
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-07 04:40:46 +0700

    Add response interceptor to handle token refresh on 401 errors

--- app/plugins/01.api.js
+++ app/plugins/01.api.js
@@ -3,7 +3,7 @@ import { useAuth } from "~/composables/useAuth";
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   const { _clearAuth, token } = useAuth();
-  
+
   const api = axios.create({
     baseURL: "https://api.ubmager.bornhub.cloud",
     headers: {
@@ -15,6 +15,43 @@ export default defineNuxtPlugin(async (nuxtApp) => {
   if (token().value) {
     api.defaults.headers.common["Authorization"] = `Bearer ${token().value}`;
   }
+  await api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      const originalRequest = error.config;
+
+      if (
+        error.response?.status === 401 &&
+        !originalRequest._retry &&
+        originalRequest.url !== "/api/refresh"
+      ) {
+        originalRequest._retry = true;
+
+        try {
+          console.log("Interceptor: Access token expired. Refreshing token...");
+
+          const response = await api.post("/api/refresh");
+          const newAccessToken = response.data.access_token;
+          token().value = newAccessToken;
+          api.defaults.headers.common[
+            "Authorization"
+          ] = `Bearer ${newAccessToken}`;
+          originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+          return api(originalRequest);
+        } catch (refreshError) {
+          console.error(
+            "Interceptor: Gagal refresh token. Logout.",
+            refreshError
+          );
+          _clearAuth();
+          return Promise.reject(refreshError);
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
 
   return {
     provide: {
