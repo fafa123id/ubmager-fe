e0f9d0e1f7b1b6bab25331d3b6f3fdc8cb8b65eb
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-06 19:13:02 +0700

    Refactor auth callback to use hash for token retrieval instead of query parameters

--- app/pages/auth/callback/index.vue
+++ app/pages/auth/callback/index.vue
@@ -8,18 +8,27 @@ import { onMounted } from "vue";
 
 definePageMeta({ auth: "guest" });
 onMounted(async () => {
-  const route = useRoute();
-  const query = route.query;
-  const { loginWithToken } = useAuth();
-  if (query.token) {
-    await loginWithToken(query.token);
-  }
-  let destination = "/";
-  if (query.next) {
-    destination = query.next;
-  }
-  setTimeout(() => {
-    navigateTo(destination);
-  }, 500);
+  const route = useRoute();
+  const { loginWithToken } = useAuth();
+
+  const hash = route.hash.substring(1);
+
+  const params = new URLSearchParams(hash);
+  const token = params.get('token');
+
+  if (token) {
+
+    await loginWithToken(token); 
+  }
+
+  let destination = "/";
+
+  if (route.query.next) {
+    destination = route.query.next;
+  }
+
+  setTimeout(() => {
+    navigateTo(destination);
+  }, 500);
 });
 </script>
