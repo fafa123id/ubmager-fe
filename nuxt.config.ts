18ca3c5748c78319da85658177434a045022381e
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-06 19:04:22 +0700

    Remove unused passport client credentials from runtime configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,8 +28,6 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE,
-      passportClientId: process.env.PASSPORT_PASSWORD_GRANT_CLIENT_ID,
-      passportClientSecret: process.env.PASSPORT_PASSWORD_GRANT_CLIENT_SECRET,
     },
   },
 });
