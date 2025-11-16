ec6e114501c83f8ee4810c5f725b8f4af9e87762
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-17 06:52:05 +0700

    Refactor auth and guest middleware for improved user detection and redirection logic

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,18 +1,13 @@
-import { useAuth } from "~/composables/useAuth";
+export default defineNuxtRouteMiddleware(async (to) => {
+  const { user, checkAuth } = useAuth();
 
-export default defineNuxtRouteMiddleware((to, from) => {
-  // Prevent SSR hydration mismatch
-  if (process.server) return;
-
-  const { user, isLoggedIn } = useAuth();
-
-  // Convert cookie string → boolean
-  const loggedIn = isLoggedIn.value === "true" || isLoggedIn.value === true;
-
-  const next = to.query.next | undefined;
-
-  if (loggedIn && user.value) {
-    console.log("Middleware [guest]: User detected in state. Redirecting to home.");
-    return navigateTo(next || "/", { replace: true });
+  const { isLoggedIn } = useAuth();
+  if (isLoggedIn.value === true) {
+    return;
   }
+  if (!user.value) {
+    return navigateTo(`/auth/login?next=${encodeURIComponent(to.fullPath)}`, {
+      replace: true,
+    });
+  }   
 });
--- app/middleware/guest.js
+++ app/middleware/guest.js
@@ -1,18 +1,18 @@
 import { useAuth } from "~/composables/useAuth";
 
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  const { user, checkAuth } = useAuth();
-  const route = useRoute();
-  const next = route.query.next
+export default defineNuxtRouteMiddleware((to, from) => {
+  // Prevent SSR hydration mismatch
+  if (process.server) return;
 
-  const { isLoggedIn } = useAuth();
-  if (!isLoggedIn.value === true) {
-    return;
-  }
-  if (user.value) {
-    console.log(
-      "Middleware [guest]: User detected in state. Redirecting to home."
-    );
-    return navigateTo(next ? next : "/", { replace: true });
+  const { user, isLoggedIn } = useAuth();
+
+  // Convert cookie string → boolean
+  const loggedIn = isLoggedIn.value === "true" || isLoggedIn.value === true;
+
+  const next = to.query.next | undefined;
+
+  if (loggedIn && user.value) {
+    console.log("Middleware [guest]: User detected in state. Redirecting to home.");
+    return navigateTo(next || "/", { replace: true });
   }
 });
