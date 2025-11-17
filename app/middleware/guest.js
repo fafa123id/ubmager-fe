2a51700af1857a0b47b01f6efe65a48de116ecd9
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-17 07:00:48 +0700

    Refactor guest middleware to simplify redirection logic and remove SSR hydration check

--- app/middleware/guest.js
+++ app/middleware/guest.js
@@ -1,16 +1,14 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  // Prevent SSR hydration mismatch
-  if (process.server) return;
+
 
   const { user, isLoggedIn } = useAuth();
 
   // Convert cookie string → boolean
   const loggedIn = isLoggedIn.value === "true" || isLoggedIn.value === true;
 
-  const next = to.query.next | undefined;
-
   if (loggedIn && user.value) {
     console.log("Middleware [guest]: User detected in state. Redirecting to home.");
-    return navigateTo(next || "/", { replace: true });
+    return navigateTo( "/", { replace: true });
   }
 });
+  
