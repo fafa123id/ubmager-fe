87b2cf70b8efa075640ddd61681e935bd5248cd0
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-17 07:49:25 +0700

    Update guest middleware to allow dynamic redirection based on query parameters

--- app/middleware/guest.global.js
+++ app/middleware/guest.global.js
@@ -5,13 +5,13 @@ export default defineNuxtRouteMiddleware((to, from) => {
   }
 
   const { user, isLoggedIn } = useAuth();
-
+  const next = to.query.next || "/";
   // Convert cookie string → boolean
   const loggedIn = isLoggedIn.value === "true" || isLoggedIn.value === true;
 
   if (loggedIn && user.value) {
     console.log("Middleware [guest]: User detected in state. Redirecting to home.");
-    return navigateTo( "/", { replace: true });
+    return navigateTo( next, { replace: true });
   }
 });
   
