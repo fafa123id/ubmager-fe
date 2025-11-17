c5076ce9742c517e2869182452b85019894ee3e9
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-18 02:39:35 +0700

    Add error and success message handling on page load

--- app/app.vue
+++ app/app.vue
@@ -5,3 +5,17 @@
     </NuxtLayout>
   </div>
 </template>
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const error = useRoute().query.error;
+  if (error) {
+    useSwal().showError(decodeURIComponent(error));
+  }
+  const success = useRoute().query.success;
+  if (success) {
+    useSwal().showSuccess(decodeURIComponent(success));
+  }
+});
+</script>
