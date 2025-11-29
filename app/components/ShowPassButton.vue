cb760588bd1bd89a1b5ce1d790ed87821d0baa64
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-29 14:10:46 +0700

    Add ShowPassButton component for password visibility toggle in ChangePw form

---
+++ app/components/ShowPassButton.vue
@@ -0,0 +1,53 @@
+<script setup>
+const props = defineProps({
+  showPass: {
+    type: Boolean,
+    required: true,
+  },
+  id : {
+    type: String,
+    default: null,
+  },
+});
+const emit = defineEmits(["toggleShow"]);
+const toggleShow = () => {
+  emit("toggleShow");
+};
+</script>
+<template>
+  <button
+    :id="id"
+    type="button"
+    @click="toggleShow"
+    class="cursor-pointer absolute inset-y-0 right-0 grid w-10 place-items-center text-slate-300 hover:text-white"
+    aria-label="Tampilkan/sem­bunyikan sandi"
+  >
+    <svg
+      v-if="!showPass"
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-5 w-5"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        stroke-width="1.5"
+        d="M2 12s3.5-7 10-7 10 7 10 7-3.5 7-10 7S2 12 2 12Z"
+      />
+      <circle cx="12" cy="12" r="3" stroke="currentColor" stroke-width="1.5" />
+    </svg>
+    <svg
+      v-else
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-5 w-5"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        stroke="currentColor"
+        stroke-width="1.5"
+        d="M3 3l18 18M10.6 10.6a3 3 0 104.2 4.2M6.1 6.1C3.8 7.7 2 12 2 12s3.5 7 10 7c2 0 3.7-.5 5.1-1.2M17.8 6.6C16.1 5.6 14.2 5 12 5"
+      />
+    </svg>
+  </button>
+</template>
--- app/components/form/ChangePw.vue
+++ app/components/form/ChangePw.vue
@@ -16,6 +16,18 @@ const loading = ref(false);
 const oldPasswordInput = ref("");
 const passwordInput = ref("");
 const passwordConfirmationInput = ref("");
+const showPassword = ref(false);
+const toggleShowPassword = () => {
+  showPassword.value = !showPassword.value;
+};
+const showPasswordConfirmation = ref(false);
+const toggleShowPasswordConfirmation = () => {
+  showPasswordConfirmation.value = !showPasswordConfirmation.value;
+};
+const showOldPassword = ref(false);
+const toggleShowOldPassword = () => {
+  showOldPassword.value = !showOldPassword.value;
+};
 const canSave = computed(() => {
   return (
     !!oldPasswordInput.value &&
@@ -59,6 +71,9 @@ const closeAndReset = () => {
   passwordInput.value = "";
   passwordConfirmationInput.value = "";
   errorMsg.value = {};
+  showOldPassword.value = false;
+  showPassword.value = false;
+  showPasswordConfirmation.value = false;
 };
 </script>
 <template>
@@ -72,15 +87,23 @@ const closeAndReset = () => {
             <label class="mb-1 block text-xs text-slate-300"
               >Password Lama</label
             >
-            <input
-              v-model="oldPasswordInput"
-              type="password"
-              class="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-2.5 text-sm text-slate-100 placeholder:text-slate-400 outline-none ring-1 ring-transparent focus:ring-sky-400 disabled:opacity-60"
-              :disabled="loading"
-              placeholder="Password Lama"
-              @focus="clearError('old_password')"
-              autocomplete="current-old-password"
-            />
+            <div class="relative">
+              <input
+                v-model="oldPasswordInput"
+                :type="showOldPassword ? 'text' : 'password'"
+                class="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-2.5 text-sm text-slate-100 placeholder:text-slate-400 outline-none ring-1 ring-transparent focus:ring-sky-400 disabled:opacity-60"
+                :disabled="loading"
+                placeholder="Password Lama"
+                @focus="clearError('old_password')"
+                autocomplete="current-old-password"
+              />
+              <ShowPassButton
+                :id="'old-password-show-pass-button'"
+                :showPass="showOldPassword"
+                @toggleShow="toggleShowOldPassword"
+              />
+            </div>
+
             <p
               v-if="errorMsg?.old_password"
               class="rounded-lg border border-rose-500/30 bg-rose-500/10 px-3 py-2 text-sm text-rose-200 mt-2"
@@ -91,15 +114,22 @@ const closeAndReset = () => {
 
           <div>
             <label class="mb-1 block text-xs text-slate-300">Password</label>
-            <input
-              v-model="passwordInput"
-              type="password"
-              class="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-2.5 text-sm text-slate-100 placeholder:text-slate-400 outline-none ring-1 ring-transparent focus:ring-sky-400 disabled:opacity-60"
-              :disabled="loading"
-              placeholder="Password"
-              @focus="clearError('password')"
-              autocomplete="current-password"
-            />
+            <div class="relative">
+              <input
+                v-model="passwordInput"
+                :type="showPassword ? 'text' : 'password'"
+                class="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-2.5 text-sm text-slate-100 placeholder:text-slate-400 outline-none ring-1 ring-transparent focus:ring-sky-400 disabled:opacity-60"
+                :disabled="loading"
+                placeholder="Password"
+                @focus="clearError('password')"
+                autocomplete="current-password"
+              />
+              <ShowPassButton
+                :id="'password-show-pass-button'"
+                :showPass="showPassword"
+                @toggleShow="toggleShowPassword"
+              />
+            </div>
             <p
               v-if="errorMsg?.password"
               class="rounded-lg border border-rose-500/30 bg-rose-500/10 px-3 py-2 text-sm text-rose-200 mt-2"
@@ -112,15 +142,22 @@ const closeAndReset = () => {
             <label class="mb-1 block text-xs text-slate-300"
               >Password Confirmation</label
             >
-            <input
-              v-model="passwordConfirmationInput"
-              type="password"
-              class="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-2.5 text-sm text-slate-100 placeholder:text-slate-400 outline-none ring-1 ring-transparent focus:ring-sky-400 disabled:opacity-60"
-              :disabled="loading"
-              placeholder="Password Confirmation"
-              @focus="clearError('password_confirmation')"
-              autocomplete="current-password_confirmation"
-            />
+            <div class="relative">
+              <input
+                v-model="passwordConfirmationInput"
+                :type="showPasswordConfirmation ? 'text' : 'password'"
+                class="w-full rounded-xl border border-white/10 bg-white/5 px-4 py-2.5 text-sm text-slate-100 placeholder:text-slate-400 outline-none ring-1 ring-transparent focus:ring-sky-400 disabled:opacity-60"
+                :disabled="loading"
+                placeholder="Password Confirmation"
+                @focus="clearError('password_confirmation')"
+                autocomplete="current-password_confirmation"
+              />
+              <ShowPassButton
+                :id="'password-confirmation-show-pass-button'"
+                :showPass="showPasswordConfirmation"
+                @toggleShow="toggleShowPasswordConfirmation"
+              />
+            </div>
             <p
               v-if="errorMsg?.password_confirmation"
               class="rounded-lg border border-rose-500/30 bg-rose-500/10 px-3 py-2 text-sm text-rose-200 mt-2"
