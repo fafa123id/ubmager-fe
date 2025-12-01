9eba2a1f1c00cbf401c7c61f3f733d2366e7620a
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-12-01 12:37:36 +0700

    Remove reset password token cookie handling in guest middleware

--- app/middleware/guest.js
+++ app/middleware/guest.js
@@ -6,8 +6,6 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const loggedIn = isLoggedIn.value === "true" || isLoggedIn.value === true;
 
   if (loggedIn || user.value) {
-    const resetCookie = useCookie("reset_password_token");
-    resetCookie.value = null;
     console.log("Middleware [guest]: User detected in state. Redirecting to home.");
     return navigateTo( next, { replace: true });
   }
